03a785b9ffca404860e65cfe4deca95cf44c9e94
root <root@ALA-IZEPILOVSKI.>
2025-07-23 13:19:57 -0400

    Configuração basica e django-rq

--- api/settings.py
+++ api/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,13 +108,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Cuiaba'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -122,3 +126,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+RQ_QUEUES = {
+    'default': {
+        'HOST': os.environ['HOST_REDIS'], # Ou o IP do seu servidor Redis
+        'PORT': os.environ['PORT_REDIS'],
+        'DB': os.environ['DB_REDIS'],
+        'PASSWORD': os.environ['PASSWORD_REDIS'], # Se o seu Redis tiver senha
+        'DEFAULT_TIMEOUT': os.environ['DEFAULT_TIMEOUT_REDIS'], # Tempo limite padrão para tarefas (em segundos)
+    }
+}
