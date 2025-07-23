5de34cc41408b6816a55d10559266b6410ed8a18
root <root@ALA-IZEPILOVSKI.>
2025-07-23 13:15:21 -0400

    Liberado hosts

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*3lgzd1xxf-+pt12!6guxg%_l!u$1=^ep86+c!$h^(=ztbrzzs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
